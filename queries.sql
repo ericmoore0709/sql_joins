f57b73af38b33deb08a278d1d6e8528bcbd4cf2a
Eric Moore <ericmoore0709@gmail.com>
2024-04-13 02:26:58 -0700

    full attempt (pre solution)

--- queries.sql
+++ queries.sql
@@ -1,1 +1,3 @@
--- write your queries here
+select * from vehicles v full join owners o on o.id = v.owner_id;
+select o.first_name, o.last_name, count(v.*) from vehicles v join owners o on o.id = v.owner_id order by o.first_name asc;
+select o.first_name, o.last_name, avg(v.price), count(v.*) from vehicles v join owners o on o.id = v.owner_id group by o.owner_id where count(v.*) > 1 and avg(v.price) > 10000 order by o.first_name desc;
