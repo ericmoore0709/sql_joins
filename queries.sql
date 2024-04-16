407d11b773d398ef3b7ee40aa80d2d76fdc35ab7
Eric Moore <ericmoore0709@gmail.com>
2024-04-16 14:24:49 -0700

    Made typo fixes in statements.

--- queries.sql
+++ queries.sql
@@ -1,3 +1,3 @@
 select * from vehicles v full join owners o on o.id = v.owner_id;
-select o.first_name, o.last_name, count(v.*) from vehicles v join owners o on o.id = v.owner_id order by o.first_name asc;
-select o.first_name, o.last_name, avg(v.price), count(v.*) from vehicles v join owners o on o.id = v.owner_id group by o.owner_id where count(v.*) > 1 and avg(v.price) > 10000 order by o.first_name desc;
+select o.first_name, o.last_name, count(v.*) from vehicles v join owners o on o.id = v.owner_id group by o.first_name, o.last_name order by o.first_name asc;
+select o.first_name, o.last_name, avg(v.price), count(v.*) from vehicles v join owners o on o.id = v.owner_id group by o.id having count(v.*) > 1 and avg(v.price) > 10000 order by o.first_name desc;
